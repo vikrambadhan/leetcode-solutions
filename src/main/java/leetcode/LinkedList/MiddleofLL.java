55e4c441a62084d106f9783cd4248b3d52dab2b4
vikram.badhan <vikram.badhan@gmail.com>
2026-02-11 20:03:55 +0530

    Middle of LL

---
+++ src/main/java/leetcode/LinkedList/MiddleofLL.java
@@ -0,0 +1,15 @@
+package main.java.leetcode.LinkedList;
+
+public class MiddleofLL {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next !=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
