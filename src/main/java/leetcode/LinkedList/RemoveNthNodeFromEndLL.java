da136bc7dd6c7cb2ffc13f00449fc493fb2a7469
vikram.badhan <vikram.badhan@gmail.com>
2026-02-13 18:14:26 +0530

    Remove nth node from end of LL

---
+++ src/main/java/leetcode/LinkedList/RemoveNthNodeFromEndLL.java
@@ -0,0 +1,25 @@
+package main.java.leetcode.LinkedList;
+
+public class RemoveNthNodeFromEndLL {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+
+        dummy.next = head;
+
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        for(int i = 0; i<n; i++){
+            fast = fast.next;
+        }
+
+        while(fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+}
