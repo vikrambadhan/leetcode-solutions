d4b3d35eb86b7e98cf9c051c544ad6c77b8acbb1
vikram.badhan <vikram.badhan@gmail.com>
2026-02-24 11:50:37 +0530

    Min Stack

---
+++ src/main/java/leetcode/stacks/MinStack.java
@@ -0,0 +1,43 @@
+package main.java.leetcode.stacks;
+
+import java.util.Stack;
+
+public class MinStack {
+    public class MinStack1 {
+
+        Stack<Integer> stack;
+        Stack<Integer> minStack;
+
+        public MinStack1() {
+            stack = new Stack<>();
+            minStack = new Stack<>();
+        }
+
+        public void push(int val) {
+            stack.push(val);
+
+            // If minStack is empty or new value is less than equal to
+            // the top of the minStack, push it onto the minStack
+            if (minStack.isEmpty() || val <= minStack.peek())
+                minStack.push(val);
+        }
+
+        public void pop() {
+            int poppedValue = stack.pop();
+
+            // If popped value == top of the minStack,
+            // pop it from the minStack as well
+            if (poppedValue == minStack.peek())
+                minStack.pop();
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public int getMin() {
+            return minStack.peek();
+        }
+    }
+
+}
