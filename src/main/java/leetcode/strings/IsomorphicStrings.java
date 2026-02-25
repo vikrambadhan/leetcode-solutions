43e8e81153bba8cba68f5e1c0c837d5f1e5100ee
vikram.badhan <vikram.badhan@gmail.com>
2026-02-25 13:22:21 +0530

    Isomorphic Strings

---
+++ src/main/java/leetcode/strings/IsomorphicStrings.java
@@ -0,0 +1,34 @@
+package main.java.leetcode.strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Character> mapST = new HashMap<>();
+        Map<Character, Character> mapTS = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+
+            // Check mapping from s to t
+            if (mapST.containsKey(charS)) {
+                if (mapST.get(charS) != charT) return false;
+            } else {
+                mapST.put(charS, charT);
+            }
+
+            // Check mapping from t to s
+            if (mapTS.containsKey(charT)) {
+                if (mapTS.get(charT) != charS) return false;
+            } else {
+                mapTS.put(charT, charS);
+            }
+        }
+
+        return true;
+    }
+}
