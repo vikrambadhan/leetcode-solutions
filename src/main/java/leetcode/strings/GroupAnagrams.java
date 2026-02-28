ab5d322f2b61345a4e307697afe6400942bce09c
vikram.badhan <vikram.badhan@gmail.com>
2026-02-28 10:18:24 +0530

    Group Anagrams

---
+++ src/main/java/leetcode/strings/GroupAnagrams.java
@@ -0,0 +1,22 @@
+package main.java.leetcode.strings;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String word : strs){
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList<>());
+            }
+
+            map.get(key).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
