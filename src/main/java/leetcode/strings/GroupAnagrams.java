9762d58b4ad0af7e2ddef018feef07891817b76a
vikram.badhan <vikram.badhan@gmail.com>
2026-03-01 10:42:38 +0530

    Group Anagrams optimised solution

--- src/main/java/leetcode/strings/GroupAnagrams.java
+++ src/main/java/leetcode/strings/GroupAnagrams.java
@@ -20,3 +20,33 @@ public class GroupAnagrams {
         return new ArrayList<>(map.values());
     }
 }
+
+//Optimal Solution
+public List<List<String>> groupAnagrams1(String[] strs) {
+
+    Map<String, List<String>> mp = new HashMap<>();
+
+    for (String word : strs) {
+
+        int[] count = new int[26];
+
+        for (char c : word.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        StringBuilder keyBuilder = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            keyBuilder.append("#").append(count[i]);
+        }
+
+        String key = keyBuilder.toString();
+
+        if (!mp.containsKey(key)) {
+            mp.put(key, new ArrayList<>());
+        }
+
+        mp.get(key).add(word);
+    }
+
+    return new ArrayList<>(mp.values());
+}
