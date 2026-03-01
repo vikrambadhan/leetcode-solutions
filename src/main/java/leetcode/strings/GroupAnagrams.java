bdf41735b4db9abfd87c7e9d4a4f613f33688944
vikram.badhan <vikram.badhan@gmail.com>
2026-03-01 10:46:59 +0530

    Group Anagrams optimised solution

--- src/main/java/leetcode/strings/GroupAnagrams.java
+++ src/main/java/leetcode/strings/GroupAnagrams.java
@@ -19,34 +19,34 @@ public class GroupAnagrams {
         }
         return new ArrayList<>(map.values());
     }
-}
 
 //Optimal Solution
-public List<List<String>> groupAnagrams1(String[] strs) {
+    public List<List<String>> groupAnagrams1(String[] str) {
 
-    Map<String, List<String>> mp = new HashMap<>();
+        Map<String, List<String>> mp = new HashMap<>();
 
-    for (String word : strs) {
+        for (String word : str) {
 
-        int[] count = new int[26];
+            int[] count = new int[26];
 
-        for (char c : word.toCharArray()) {
-            count[c - 'a']++;
+            for (char c : word.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            StringBuilder keyBuilder = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                keyBuilder.append("#").append(count[i]);
+            }
+
+            String key = keyBuilder.toString();
+
+            if (!mp.containsKey(key)) {
+                mp.put(key, new ArrayList<>());
+            }
+
+            mp.get(key).add(word);
         }
 
-        StringBuilder keyBuilder = new StringBuilder();
-        for (int i = 0; i < 26; i++) {
-            keyBuilder.append("#").append(count[i]);
-        }
-
-        String key = keyBuilder.toString();
-
-        if (!mp.containsKey(key)) {
-            mp.put(key, new ArrayList<>());
-        }
-
-        mp.get(key).add(word);
+        return new ArrayList<>(mp.values());
     }
-
-    return new ArrayList<>(mp.values());
 }
