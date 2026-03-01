6440baea8a5a1575b1a897134d3cdbb6bd0b7793
vikram.badhan <vikram.badhan@gmail.com>
2026-03-01 12:32:39 +0530

    Two Sum II

---
+++ src/main/java/leetcode/arrays/TwoSumII.java
@@ -0,0 +1,21 @@
+package main.java.leetcode.arrays;
+
+public class TwoSumII {
+    public int[] twoSum(int[] numbers, int target) {
+        int left =0;
+        int right = numbers.length - 1;
+
+        while(left < right){
+            int sum = numbers[left] + numbers[right];
+
+            if(sum < target){
+                left ++;
+            }else if(sum > target){
+                right --;
+            }else{
+                return new int[] { left + 1, right + 1};
+            }
+        }
+        return null;
+    }
+}
