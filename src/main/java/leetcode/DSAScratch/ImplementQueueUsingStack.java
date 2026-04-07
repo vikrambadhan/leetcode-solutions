01888ca69d76ccd78cc638fab077664ac1e80103
vikram.badhan <vikram.badhan@gmail.com>
2026-04-07 13:34:46 +0530

    Implement Queue Using Stack

---
+++ src/main/java/leetcode/DSAScratch/ImplementQueueUsingStack.java
@@ -0,0 +1,35 @@
+package main.java.leetcode.DSAScratch;
+
+import java.util.Stack;
+
+public class ImplementQueueUsingStack {
+    private Stack<Integer> input;
+    private Stack<Integer> output;
+
+    public ImplementQueueUsingStack() {
+        input = new Stack<>();
+        output = new Stack<>();
+    }
+
+    public void push(int x) {
+        input.push(x);
+    }
+
+    public int pop() {
+        peek();
+        return output.pop();
+    }
+
+    public int peek() {
+        if(output.empty()){
+            while(!input.empty()){
+                output.push(input.pop());
+            }
+        }
+        return output.peek();
+    }
+
+    public boolean empty() {
+        return input.empty() && output.empty();
+    }
+}
