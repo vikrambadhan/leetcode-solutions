360be5973e6c227f032621beb3173f0ddba0a9f2
vikram.badhan <vikram.badhan@gmail.com>
2026-04-07 11:10:23 +0530

    Evaluate reverse polish Notation

---
+++ src/main/java/leetcode/stacks/EvalReversePolishNotation.java
@@ -0,0 +1,33 @@
+package main.java.leetcode.stacks;
+
+import java.util.Stack;
+
+public class EvalReversePolishNotation {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for(String token : tokens){
+            if(token.equals("+")){
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a + b);
+            }else if(token.equals("-")){
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a - b);
+            }else if(token.equals("*")){
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a * b);
+            }else if(token.equals("/")){
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a / b);
+            }else{
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.peek();
+    }
+}
