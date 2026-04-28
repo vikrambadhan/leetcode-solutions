4488c07238f370877b6c6b1a2da1559fc1ea5a0e
vikram.badhan <vikram.badhan@gmail.com>
2026-04-28 18:25:26 +0530

    Balanced Binary Tree

---
+++ src/main/java/leetcode/binarytree/BalancedBinaryTree.java
@@ -0,0 +1,19 @@
+package main.java.leetcode.binarytree;
+
+public class BalancedBinaryTree {
+    public boolean isBalanced(TreeNode root) {
+        return check(root) != -1;
+    }
+    public int check(TreeNode node){
+        if(node == null) return 0;
+
+        int left = check(node.left);
+        if(left == -1) return -1;
+
+        int right = check(node.right);
+        if(right == -1) return -1;
+
+        if(Math.abs(left-right) > 1) return -1;
+        return 1 + Math.max(left, right);
+    }
+}
